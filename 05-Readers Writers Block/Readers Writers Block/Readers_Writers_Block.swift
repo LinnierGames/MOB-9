a930d337058b2394934d51733bb3bc1bef399ddf
LinnierGames <esericksanc@gmail.com>
2018-05-21 15:25:44 -0700

    init commit

---
+++ 05-Readers Writers Block/Readers Writers Block/Readers_Writers_Block.swift
@@ -0,0 +1,34 @@
+//
+//  Readers_Writers_Block.swift
+//  Readers Writers Block
+//
+//  Created by Erick Sanchez on 5/21/18.
+//
+
+import XCTest
+
+class Readers_Writers_Block: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
